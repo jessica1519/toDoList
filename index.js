78f940651f91f30c025b219cbd0b6bb76251f78a
jessica1519 <marcianojessica90@gmail.com>
2023-10-17 18:46:19 +0200

    remove port

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
